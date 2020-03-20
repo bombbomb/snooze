28a22f72029d39527b0429adeb8cd778f356ebcf
briankopp <briankopp.usa@gmail.com>
2020-03-20 06:34:40 -0600

    Clean up index file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-var forever         = require('forever');
-var urlHelper       = require('url');
-var _               = require('underscore');
-var guid            = require('guid');
-var bbJWT           = require("bbjwt-client");
-
-var AWS             = require('aws-sdk');
-var crypto          = require('crypto');
-
 const {
     configurePipelineMetricSender,
-    incrementPipelineSubMetric,
     logger
 } = require('@bblabs/knapsack');
 
-var snsMap          = require('./core/snsMap');
-var tasks           = require('./core/tasks');
-
-var runner          = require('./core/runner');
-var user            = require('./util/user');
 const { sqsProcessFunction } = require('./sqs');
 const { app } = require('./app');
 
@@ -48,8 +33,6 @@ app.listen(port, (err) => {
     }
 });
 
-//var SERVERID = uuid.v4();
-
 process.on('uncaughtException',function(err){
     try {
         if (err.message.indexOf('ECONNRESET') == -1) {
@@ -59,32 +42,5 @@ process.on('uncaughtException',function(err){
         console.error('[INDEX] uncaughtException Exception '+e);
     }
 });
-
-
-/* start task runner process */
-
-function runnerExited()
-{
-    logger.error('WARNING: Snooze main runner exited! Was this expected?');
-}
-function runnerStarted()
-{
-    console.log('Task Runner Started');
-}
-
-// var child = new(forever.Forever)('core/runner.js', {
-//     max: 3,
-//     silent: true,
-//     args: []
-// });
-
-// child.on('start', runnerStarted);
-// child.on('exit', runnerExited);
-// child.start();
-
-for (var i = 0; i < 3; i++) {
-    console.log('starting runner...');
-    var f = require('./core/runner.js');
-}
 
 logger.info('Snooze Started Successfully!');
